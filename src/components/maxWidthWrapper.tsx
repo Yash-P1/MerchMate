55ce4d3d9695aabeb1befe79890c25bf1ef824ed
Yash Patel <yspatel97@gmail.com>
2024-01-29 05:42:28 -0500

    Landing page setup

---
+++ src/components/maxWidthWrapper.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    // used cn as default styling wrapper for components, with a dynamic className for new changes depending on component
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
