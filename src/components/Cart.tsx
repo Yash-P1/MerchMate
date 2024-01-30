670ed3e27a2db4180f5e3ec28db27681d0fce770
Yash Patel <yspatel97@gmail.com>
2024-01-29 20:24:41 -0500

    Added Navbar

---
+++ src/components/Cart.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Sheet, ShoppingCart } from "lucide-react";
+import React from "react";
+import { SheetTrigger } from "./ui/sheet";
+
+export default function Cart() {
+  return (
+    <div>Yash</div>
+  );
+}
